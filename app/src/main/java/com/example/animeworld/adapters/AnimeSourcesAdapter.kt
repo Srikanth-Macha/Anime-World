422a042a09e276adc26799f51d4d6be196a4fea1
Srikanth-Macha <srikanthmacha32@gmail.com>
2022-06-29 22:56:54 +0530

    Added clickable URLs

---
+++ app/src/main/java/com/example/animeworld/adapters/AnimeSourcesAdapter.kt
@@ -0,0 +1,54 @@
+package com.example.animeworld.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.net.Uri
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.animeworld.R
+
+class AnimeSourcesAdapter(
+    private val context: Context,
+    private val sources: List<String>,
+) : RecyclerView.Adapter<SourcesViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SourcesViewHolder {
+        val view = LayoutInflater.from(context)
+            .inflate(R.layout.anime_sources_item, parent, false)
+
+        return SourcesViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: SourcesViewHolder, position: Int) {
+        holder.animeSourceItem.apply {
+            text = sources[position]
+            setTextColor(Color.parseColor("#EC5222"))
+        }
+        holder.onItemClick(context)
+    }
+
+    override fun getItemCount(): Int = sources.size
+
+}
+
+class SourcesViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    val animeSourceItem: TextView = view.findViewById(R.id.animeSourceItemID)
+
+    fun onItemClick(context: Context) {
+        animeSourceItem.setOnClickListener { clickedItem ->
+            val sourceItem = clickedItem as TextView
+
+            val sourceItemText = sourceItem.text
+            sourceItem.setBackgroundColor(Color.parseColor("#FF2196F3"))
+
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.data = Uri.parse(sourceItemText.toString())
+            context.startActivity(intent)
+        }
+    }
+
+}
--- app/src/main/java/com/example/animeworld/ui/AnimeInfoActivity.kt
+++ app/src/main/java/com/example/animeworld/ui/AnimeInfoActivity.kt
@@ -1,11 +1,14 @@
 package com.example.animeworld.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.example.animeworld.adapters.AnimeSourcesAdapter
 import com.example.animeworld.databinding.ActivityAnimeInfoBinding
 import com.example.animeworld.models.Anime
 import com.example.animeworld.viewmodels.AnimeViewModel
@@ -13,6 +16,7 @@ import com.example.animeworld.viewmodels.AnimeViewModel
 class AnimeInfoActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAnimeInfoBinding
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAnimeInfoBinding.inflate(layoutInflater)
@@ -33,16 +37,7 @@ class AnimeInfoActivity : AppCompatActivity() {
                     airingDates.text.toString() + animeResponse.startDate + " to " + animeResponse.endDate
                 description.text = animeResponse.description
 
-                var sources = ""
-                val sourcesList = anime.sources
-                for (i in 0 until sourcesList?.size!!) {
-                    sources += "${sourcesList[i]} \n"
-
-                    if (i == 10)
-                        break
-                }
-
-                animeSources.text = sources
+                anime.sources?.let { setSourcesRecyclerView(it) }
 
                 animeProgressBar.visibility = View.GONE
                 enableBackgroundInteraction()
@@ -73,10 +68,19 @@ class AnimeInfoActivity : AppCompatActivity() {
 
     private fun disableBackgroundInteraction() {
         window.setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
-            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
     }
 
     private fun enableBackgroundInteraction() {
-        window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE)
+    }
+
+    private fun setSourcesRecyclerView(sources: List<String>) {
+        binding.animeSourcesRecyclerView.apply {
+            adapter = AnimeSourcesAdapter(this@AnimeInfoActivity, sources)
+            layoutManager = object : LinearLayoutManager(this@AnimeInfoActivity) {
+                override fun canScrollVertically(): Boolean = false
+            }
+        }
     }
 }
