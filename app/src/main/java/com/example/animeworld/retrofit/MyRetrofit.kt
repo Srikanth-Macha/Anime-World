1f930501283f6544d082e553e454f721ec506d4a
Srikanth-Macha <srikanthmacha32@gmail.com>
2022-07-22 11:06:28 +0530

    login screen changes

--- app/src/main/java/com/example/animeworld/retrofit/MyRetrofit.kt
+++ app/src/main/java/com/example/animeworld/retrofit/MyRetrofit.kt
@@ -8,8 +8,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
 
 object MyRetrofit {
-    private const val URL = "http://192.168.43.20:3000/"
-        /*"https://anime-backend-i2xj.onrender.com/"*/
+    private const val URL = /*"http://192.168.43.20:3000/"*/
+        "https://anime-backend-i2xj.onrender.com/"
 
     fun getInstance(): Retrofit {
         return Retrofit.Builder()
