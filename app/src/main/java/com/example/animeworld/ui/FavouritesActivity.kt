4d93eac20d6851a687c7b1fa1184708274609dc3
Srikanth-Macha <srikanthmacha32@gmail.com>
2022-09-09 16:52:25 +0530

    Favourites screen added

--- app/src/main/java/com/example/animeworld/ui/FavouritesActivity.kt
+++ app/src/main/java/com/example/animeworld/ui/FavouritesActivity.kt
@@ -1,12 +1,50 @@
 package com.example.animeworld.ui
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-import com.example.animeworld.R
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.animeworld.adapters.AnimeAdapter
+import com.example.animeworld.databinding.ActivityFavouritesBinding
+import com.example.animeworld.models.Anime
+import com.example.animeworld.viewmodels.AnimeViewModel
 
 class FavouritesActivity : AppCompatActivity() {
+    lateinit var binding: ActivityFavouritesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_favourites)
+        binding = ActivityFavouritesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val userEmail = getSharedPreferences("User", Context.MODE_PRIVATE).getString("Email", null)
+
+        val viewModel = ViewModelProvider(this)[AnimeViewModel::class.java]
+        val favouritesLiveData = viewModel.getFavourites(userEmail.toString())
+
+        favouritesLiveData.observe(this) { favouritesList ->
+            setRecyclerView(favouritesList)
+        }
+
+    }
+
+    private fun setRecyclerView(favouritesList: List<Anime>?) {
+        val layoutManager = GridLayoutManager(this@FavouritesActivity, 2)
+
+        val adapter = object : AnimeAdapter(
+            favouritesList,
+            this@FavouritesActivity,
+            layoutManager,
+            this@FavouritesActivity) {
+
+            override fun loadNextPage(page: Int) {}
+        }
+
+        binding.favouritesRecyclerView.apply {
+            this.adapter = adapter
+            this.layoutManager = layoutManager
+        }
     }
 }
